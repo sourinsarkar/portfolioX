794c77f53693196e7f1c4cd8564342e94c6128cc
Sourin <iamsourinsarkar@gmail.com>
2023-11-16 22:43:03 +0530

    Completed Nav bar structure

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,28 +1,37 @@
-import { BellIcon, ScissorsIcon } from "@heroicons/react/24/outline"
-import { BellIcon as SolidBellIcon, ScissorsIcon as SolidScissorsIcon } from "@heroicons/react/24/solid"
+import { ChevronDownIcon } from "@heroicons/react/24/outline"
+// import { ChevronDown as SolidChevronDown } from "@heroicons/react/24/solid"
 
 const GlobalNav: React.FC = () => {
     return (
         <div>
-            <div>
+            <div className="flex-generic justify-between">
                 <div>
-                    <div>
+                    <div className="flex-generic">
                         <div>
                             <h1>Sourin <span>Sarkar</span></h1>
                         </div>
                         <div>
-                            
+                            <ChevronDownIcon className="h-6 w-6 text-black"/>
                         </div>
                     </div>
                 </div>
-                <div></div>
-                <div></div>
+
+                <div>
+                    <div>
+                        <ul className="flex-generic list-elements">
+                            <li><a href="/">Projects</a></li>
+                            <li><a href="/">Designs</a></li>
+                            <li><a href="/">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <button>Reach me</button>
+                    </div>
+                </div>
             </div>
-            <SolidBellIcon className="h-6 w-6 text-blue-900" />
-            <SolidScissorsIcon className="h-6 w-6 text-blue-900" />
-            <ScissorsIcon className="h-6 w-6 text-yellow-300" />
-            <BellIcon className="h-6 w-6 text-yellow-300 stroke-1" />
-            <BellIcon className="h-6 w-6 text-yellow-300 stroke-2" />
         </div>
     );
 }
