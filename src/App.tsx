ee0bf7993ed5aaa2471dd2d72c70f9c82556bd66
Sourin <iamsourinsarkar@gmail.com>
2023-11-16 19:35:51 +0530

    Testing icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Home from "./layouts/Home";
 
 function App() {
--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, ScissorsIcon } from "@heroicons/react/24/outline"
+import { BellIcon as SolidBellIcon, ScissorsIcon as SolidScissorsIcon } from "@heroicons/react/24/solid"
+
 const GlobalNav: React.FC = () => {
     return (
         <div>
@@ -15,6 +18,11 @@ const GlobalNav: React.FC = () => {
                 <div></div>
                 <div></div>
             </div>
+            <SolidBellIcon className="h-6 w-6 text-blue-900" />
+            <SolidScissorsIcon className="h-6 w-6 text-blue-900" />
+            <ScissorsIcon className="h-6 w-6 text-yellow-300" />
+            <BellIcon className="h-6 w-6 text-yellow-300 stroke-1" />
+            <BellIcon className="h-6 w-6 text-yellow-300 stroke-2" />
         </div>
     );
 }
