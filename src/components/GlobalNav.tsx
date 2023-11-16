6c922318f6e021429f32307da28d96971bce7c88
Sourin <iamsourinsarkar@gmail.com>
2023-11-17 01:43:15 +0530

    Added fonts

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -1,39 +1,45 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline"
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 // import { ChevronDown as SolidChevronDown } from "@heroicons/react/24/solid"
 
 const GlobalNav: React.FC = () => {
-    return (
+  return (
+    <div>
+      <div className="flex-generic justify-between">
         <div>
-            <div className="flex-generic justify-between">
-                <div>
-                    <div className="flex-generic">
-                        <div>
-                            <h1>Sourin <span>Sarkar</span></h1>
-                        </div>
-                        <div>
-                            <ChevronDownIcon className="h-6 w-6 text-black"/>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <ul className="flex-generic list-elements">
-                            <li><a href="/">Projects</a></li>
-                            <li><a href="/">Designs</a></li>
-                            <li><a href="/">About</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <button>Reach me</button>
-                    </div>
-                </div>
+          <div className="flex-generic">
+            <div>
+              <h1 className="primary-font">Sourin <span>Sarkar</span></h1>
             </div>
+            <div>
+              <ChevronDownIcon className="h-6 w-6 text-black" />
+            </div>
+          </div>
         </div>
-    );
-}
 
-export default GlobalNav
+        <div>
+          <div>
+            <ul className="flex-generic list-elements">
+              <li>
+                <a href="/">Projects</a>
+              </li>
+              <li>
+                <a href="/">Designs</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <button>Reach me</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GlobalNav;
