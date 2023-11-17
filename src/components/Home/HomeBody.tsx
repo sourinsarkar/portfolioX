9cc54fec4248ea03a8769c01d7c0ccb9bb3ba154
Sourin <iamsourinsarkar@gmail.com>
2023-11-17 22:41:42 +0530

    Created the home page basic structure

--- src/components/Home/HomeBody.tsx
+++ src/components/Home/HomeBody.tsx
@@ -1,6 +1,40 @@
+import { Download } from 'iconoir-react/regular';
+import PrimeImage from '../../assets/images/prime-image.jpg'
+import { Twitter, Github } from 'iconoir-react'
+
 const HomeBody: React.FC = () => {
     return (
         <div>
+            <div>
+                <div>
+                    <div>
+                        <img src={PrimeImage} alt="Photo" />
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <div>
+                            <h1>Hi there,<br />Happy to have you.</h1>
+                        </div>
+                        <div>
+                            <p>A programming enthusiast with a knack for turning concepts into captivating solutions.</p>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><Twitter /></li>
+                                <li><Github /></li>
+                                <li>
+                                    <p>Download my <span>CV <Download /></span> </p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>    
+                </div>
+            </div>
         </div> 
     );
 }
--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,10 +1,12 @@
-import GlobalNav from "../components/GlobalNav";
+import HomeBody from "../components/Home/HomeBody";
+import GlobalNav from "../components/Nav/GlobalNav";
 
 const Home: React.FC = () => {
     return (
         <div>
             <div className="mx-12 m-5">
                 <GlobalNav />
+                <HomeBody />
             </div>
         </div>
     );
