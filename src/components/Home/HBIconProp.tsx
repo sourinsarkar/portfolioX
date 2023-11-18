254959a62182d48d7d21ee54d2d3ef9f7101b2fd
Sourin <iamsourinsarkar@gmail.com>
2023-11-18 23:49:47 +0530

    Completed the web version. Much more is left

--- src/components/Home/HBIconProp.tsx
+++ src/components/Home/HBIconProp.tsx
@@ -6,10 +6,10 @@ const HBIconProp: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <IconoirProvider
       iconProps={{
-        color: "#000",
+        color: "#27282C",
         strokeWidth: 2,
-        width: "0.9em",
-        height: "0.9em",
+        width: "1.5em",
+        height: "1.5em",
       }}
     >
       {children}
--- src/components/Home/HomeBody.tsx
+++ src/components/Home/HomeBody.tsx
@@ -1,6 +1,7 @@
 import { Download } from 'iconoir-react/regular';
 import PrimeImage from '../../assets/images/prime-image.jpg'
 import { Twitter, Github } from 'iconoir-react'
+import HBIconProp from './HBIconProp';
 
 const HomeBody: React.FC = () => {
     return (
@@ -21,12 +22,12 @@ const HomeBody: React.FC = () => {
                             <p>A programming enthusiast with a knack for turning concepts into captivating solutions.</p>
                         </div>
                         <div className="my-8">
-                            <ul className="flex list-home-elements">
-                                <li><Twitter /></li>
-                                <li><Github /></li>
-                                <li className="flex">
+                            <ul className="flex items-center list-home-elements">
+                                <li><HBIconProp><Twitter /></HBIconProp></li>
+                                <li><HBIconProp><Github /></HBIconProp></li>
+                                <li className="flex items-center mono-font font-semibold">
                                     <p>Download my</p>
-                                    <button className="flex">CV <Download /></button>
+                                    <button className="flex mx-2 px-4 py-2 rounded-full bg-[#27282C] text-[#FFFAFF]">CV <HBIconProp><Download color="#FFFAFF" /></HBIconProp></button>
                                 </li>
                             </ul>
                         </div>
