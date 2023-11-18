813c23b835d9e4e32c453c99e5ae1a72543ddb8e
Sourin <iamsourinsarkar@gmail.com>
2023-11-19 00:11:47 +0530

    Issues found: HBIconProp.tsx

--- src/components/Home/HBIconProp.tsx
+++ src/components/Home/HBIconProp.tsx
@@ -1,12 +1,12 @@
 import { IconoirProvider } from "iconoir-react/solid";
 
-type ReactNode = string| number| boolean| null| undefined| ReactElement| ReactFragment| ReactPortal;
+type ReactNode = string| number| boolean| null| undefined;
 
 const HBIconProp: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <IconoirProvider
       iconProps={{
-        color: "#27282C",
+        color: "#27282",
         strokeWidth: 2,
         width: "1.5em",
         height: "1.5em",
