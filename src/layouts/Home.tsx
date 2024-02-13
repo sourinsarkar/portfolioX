3a9467ed016b17c107f96dd58590f095351a29a0
Sourin <iamsourinsarkar@gmail.com>
2024-02-13 21:06:51 +0530

    All issues fixed

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,13 +1,7 @@
-import HomeBody from "../components/Home/HomeBody";
-import GlobalNav from "../components/Nav/GlobalNav";
-
 const Home: React.FC = () => {
     return (
         <div className="max-w-7xl mx-auto">
-            <div className="h-screen flex flex-col mx-5 xl:mx-12">
-                <div className="py-5"><GlobalNav /></div>
-                <div className="flex items-center xl:grow"><HomeBody /></div>
-            </div>
+            
         </div>
     );
 }
