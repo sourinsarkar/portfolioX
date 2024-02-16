cb692a6c602f30252cb3f34c87e677f17abe5d00
Sourin <iamsourinsarkar@gmail.com>
2024-02-16 13:05:08 +0530

    Added: Mask for bigger screen

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -4,7 +4,13 @@ import Navbar from "../components/Navbar"
 const Home: React.FC = () => {
     return (
         <div className="max-w-screen-lg mx-auto">
-            <div className="mx-6">
+            <div className="sm:h-dvh hidden sm:flex justify-center items-center">
+                <div className="p-10 rounded-3xl bg-blueL-1x">
+                    <h1 className="text-center primary-font font-bold text-3.5xl tracking-tight leading-none text-blue-10x">Coming soon</h1>
+                    <p className="text-center sub-font text-blueD-10x mt-5">Website is begin worked on.<br/>For now, please visit via your mobile device.</p>
+                </div>
+            </div>
+            <div className="mx-6 sm:hidden">
                 <Navbar />
                 <Content />
             </div>
