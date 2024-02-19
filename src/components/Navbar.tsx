f4d3bbdcafa0f84b15673c28ff7f7ba45b668037
Sourin <iamsourinsarkar@gmail.com>
2024-02-20 00:53:54 +0530

    Working: Hamburger Dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import { Bars2Icon } from "@heroicons/react/24/outline";
 import SourinIcon from "../assets/images/sourin_icon_svg.svg"
+import DevBlueIcon from "../assets/icons/dev_blue_icon_svg.svg"
+import DevGreenIcon from "../assets/icons/dev_green_icon_svg.svg"
+import DevPinkIcon from "../assets/icons/dev_pink_icon_svg.svg"
 
 // Each element has overall height of 28px.
 
@@ -23,6 +26,44 @@ const Navbar: React.FC = () => {
                 <div>
                     <button hidden>Contact me</button>
                     <div className="px-3 py-1 rounded-full bg-blueL-1x"><Bars2Icon className="w-6 h-6 text-blue-10x"/></div>
+                </div>
+            </div>
+
+            {/* Dropdown */}
+            <div>
+                <div>
+                    <ul className="primary-font font-bold text-2xl tracking-tight leading-snug text-blue-10x">
+                        <li><a href="#Projects">Projects</a></li>
+                        <li><a href="#Skills">Skill Set</a></li>
+                        <li><a href="#About">A brief on me</a></li>
+                        <li><a href="#Contributions">Contributions</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p className="text-sm sub-font">Explore these</p>
+                    <ul className="space-y-4">
+                        <li className="flex">
+                            <div className="flex justify-center items-start"><img src={DevBlueIcon} alt="DevBlueIcon" className="w-7 h-7" /></div>
+                            <div>
+                                <h4 className="primary-font font-semibold text-xl tracking-tight leading-snug text-blue-10x">Currency</h4>
+                                <p className="text-sm sub-font">A tool that you’ll need handy whenever you’re on trips to different country.</p>
+                            </div>
+                        </li>
+                        <li className="flex">
+                            <div className="flex justify-center items-start"><img src={DevGreenIcon} alt="DevGreenIcon" className="w-7 h-7" /></div>
+                            <div>
+                                <h4 className="primary-font font-semibold text-xl tracking-tight leading-snug text-blue-10x">Fontground</h4>
+                                <p className="text-sm sub-font">A creative platform for designers to try and test different fonts.</p>
+                            </div>
+                        </li>
+                        <li className="flex">
+                            <div className="flex justify-center items-start"><img src={DevPinkIcon} alt="DevPinkIcon" className="w-7 h-7" /></div>
+                            <div>
+                                <h4 className="primary-font font-semibold text-xl tracking-tight leading-snug text-blue-10x">Passwords</h4>
+                                <p className="text-sm sub-font">Create, save & use strong passwords on the go. Browser extension available.</p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
